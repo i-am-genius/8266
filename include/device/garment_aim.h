9ec7996ef1fa6d2412643a5baa3f3ebc7aa3fe32
Genius <53309945+i-am-genius@users.noreply.github.com>
2026-08-11 22:32:17 +0800

    feat: aim lamp at detected garment

---
+++ include/device/garment_aim.h
@@ -0,0 +1,19 @@
+#pragma once
+
+struct GarmentAimConfig {
+  float defaultPanDeg;
+  float defaultTiltDeg;
+  float horizontalFovDeg;
+  float verticalFovDeg;
+};
+
+struct GarmentAimTarget {
+  float panDeg;
+  float tiltDeg;
+};
+
+GarmentAimTarget calculateGarmentAimTarget(
+  float normalizedCenterX,
+  float normalizedCenterY,
+  const GarmentAimConfig& config
+);
