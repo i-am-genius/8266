895bcde2c6f4c2d5f8b34c55e5906f826c3d0ad0
Genius <53309945+i-am-genius@users.noreply.github.com>
2026-08-13 10:02:23 +0800

    fix: defer non-critical HTTP during person tracking

--- test/test_wave_http_policy/test_main.cpp
+++ test/test_wave_http_policy/test_main.cpp
@@ -5,17 +5,27 @@
 void setUp() {}
 void tearDown() {}
 
-void test_allows_non_critical_http_when_wave_is_idle() {
-  TEST_ASSERT_TRUE(shouldRunNonCriticalHttp(false));
+void test_allows_non_critical_http_when_wave_and_tracking_are_idle() {
+  TEST_ASSERT_TRUE(shouldRunNonCriticalHttp(false, false));
 }
 
 void test_defers_non_critical_http_while_wave_is_running() {
-  TEST_ASSERT_FALSE(shouldRunNonCriticalHttp(true));
+  TEST_ASSERT_FALSE(shouldRunNonCriticalHttp(true, false));
+}
+
+void test_defers_non_critical_http_while_person_tracking_is_active() {
+  TEST_ASSERT_FALSE(shouldRunNonCriticalHttp(false, true));
+}
+
+void test_defers_non_critical_http_while_wave_and_tracking_are_active() {
+  TEST_ASSERT_FALSE(shouldRunNonCriticalHttp(true, true));
 }
 
 int main(int, char**) {
   UNITY_BEGIN();
-  RUN_TEST(test_allows_non_critical_http_when_wave_is_idle);
+  RUN_TEST(test_allows_non_critical_http_when_wave_and_tracking_are_idle);
   RUN_TEST(test_defers_non_critical_http_while_wave_is_running);
+  RUN_TEST(test_defers_non_critical_http_while_person_tracking_is_active);
+  RUN_TEST(test_defers_non_critical_http_while_wave_and_tracking_are_active);
   return UNITY_END();
 }
